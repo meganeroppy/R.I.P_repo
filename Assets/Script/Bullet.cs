75b08c2f6a070240d4fe2e25c7532592f3031dc7
meganeroppy <roppy@hotmail.co.jp>
2014-11-12 17:31:50 +0900

    Create brunch dev
    And Add Project files

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : Flyer {
+	protected override void ApplyHealthDamage(int value){
+		base.ApplyHealthDamage(value);
+		if (current_health <= 0) {
+			Destroy (this.gameObject);
+		} else {
+			StartCoroutine(WaitAndExecute(0.7f, false));
+		}
+	}
+	
+	protected virtual IEnumerator WaitAndExecute(float delay, bool dying){
+		yield return new WaitForSeconds (delay);
+		renderer.material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+		invincible = false;
+	}
+}
---
+++ Assets/Script/GhostKiller.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostKiller : DeadZone {
+
+	protected override void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.gameObject.tag == "Player" && !GameManager.Miss()) {
+			if(col.GetComponent<Player>().CheckIsLiving()){
+				return;
+			}
+			col.SendMessage("Miss");
+		}	}
+}
